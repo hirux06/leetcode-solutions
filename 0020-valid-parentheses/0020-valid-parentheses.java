c8c260c4bc063130111194a1b1f4ff0099be6dda
Saran Hiruthik <156305967+hirux06@users.noreply.github.com>
2024-09-26 11:55:14 +0530

    Time: 1 ms (98.56%), Space: 41.4 MB (53.1%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == ']' || ch == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                        (ch == ']' && top != '[') ||
+                        (ch == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+
+    }
+}
