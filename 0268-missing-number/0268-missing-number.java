2aba39eb81505be70ccc3dd39c74f1bbdd8d1ca1
Saran Hiruthik M <saran.hiruthik83@gmail.com>
2025-06-14 22:11:22 +0530

    Time: 0 ms (100%), Space: 45 MB (96.88%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        int totalSum = (n)*(n+1)/2;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+        }
+        return totalSum - sum;
+    }
+}
